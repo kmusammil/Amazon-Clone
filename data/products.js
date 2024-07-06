99cf94c699ef9b946b38cd0ae14017b920393c74
Musammil k <65101269+mu-za-mmil-k@users.noreply.github.com>
2024-07-06 04:21:00 +0000

    Initial commit

--- data/products.js
+++ data/products.js
@@ -1,4 +1,14 @@
-const products = [
+export function  getProduct(productId) {
+   let matchingItem;
+     products.forEach((product) => {
+       if (productId === product.id) {
+         matchingItem = product;
+       }
+     })
+     return matchingItem;
+}
+
+export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
